07c0b18d128736d48fd74ca9e86c367004752ba0
pianophage <cc.flags@gmail.com>
2015-11-28 23:08:59 -0500

    update .gitignore; reformat src/index.js; exit on --help

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
+// index.js - Entry point. From the base directory, do 'node src/index.js' to run pogar.
+
 // Imports
 var Commands = require('./modules/CommandList');
 var GameServer = require('./GameServer');
 
-// Init variables
-var showConsole = true;
-
-// Start msg
-console.log("[Game] Ogar - An open source Agar.io server implementation");
+// Show the console by default.
+var shouldShowConsole = true;
 
 // Handle arguments
-process.argv.forEach(function(val) {
-    if (val == "--noconsole") {
-        showConsole = false;
-    } else if (val == "--help") {
-        console.log("Proper Usage: node index.js");
-        console.log("    --noconsole         Disables the console");
-        console.log("    --help              Help menu.");
-        console.log("");
+process.argv.forEach(function (arg) {
+    if (arg === '--noconsole') {
+        shouldShowConsole = false;
+    } else if (arg === '--help') {
+        console.log('Proper Usage: node src/index.js');
+        console.log('    --noconsole         Disables the console');
+        console.log('    --help              Help menu.');
+        console.log('');
+
+        process.exit(0);
     }
 });
 
@@ -26,7 +27,7 @@ gameServer.start();
 // Add command handler
 gameServer.commands = Commands.list;
 // Initialize the server console
-if (showConsole) {
+if (shouldShowConsole) {
     var readline = require('readline');
     var in_ = readline.createInterface({ input: process.stdin, output: process.stdout });
     setTimeout(prompt, 100);
@@ -36,22 +37,22 @@ if (showConsole) {
 // Console functions
 
 function prompt() {
-    in_.question(">", function(str) {
+    in_.question('>', function(str) {
     	parseCommands(str);
         return prompt(); // Too lazy to learn async
-    });	
+    });
 };
 
 function parseCommands(str) {
     // Log the string
     gameServer.log.onCommand(str);
-    
+
     // Don't process ENTER
     if (str === '')
         return;
 
     // Splits the string
-    var split = str.split(" ");
+    var split = str.split(' ');
 
     // Process the first string value
     var first = split[0].toLowerCase();
@@ -61,6 +62,6 @@ function parseCommands(str) {
     if (typeof execute != 'undefined') {
         execute(gameServer,split);
     } else {
-        console.log("[Console] Invalid Command!");
+        console.log('[Console] Invalid Command!');
     }
 };
